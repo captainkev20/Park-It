3bbe4791ba62203f54c08c58b6c128235ee85a13
Kevin Walker <kevin.walker@droid.rocks>
2019-02-09 16:07:23 -0500

    PaymentFragment - renamed to NewPaymentFragment.

--- app/src/main/java/com/rocks/kevinwalker/parkit/payments/NewPaymentFragment.java
+++ app/src/main/java/com/rocks/kevinwalker/parkit/payments/NewPaymentFragment.java
@@ -1,5 +1,6 @@
 package com.rocks.kevinwalker.parkit.payments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,9 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
 import com.rocks.kevinwalker.parkit.utils.FirestoreHelper;
 import com.stripe.android.Stripe;
 import com.stripe.android.TokenCallback;
@@ -17,27 +17,38 @@ import com.stripe.android.model.Card;
 import com.stripe.android.model.Customer;
 import com.stripe.android.model.Token;
 import com.stripe.android.view.CardInputWidget;
-
-
 import com.rocks.kevinwalker.parkit.R;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class PaymentFragment extends Fragment
+public class NewPaymentFragment extends Fragment
         implements View.OnClickListener {
 
-    private static final String TAG = PaymentFragment.class.getName();
+    private static final String TAG = NewPaymentFragment.class.getName();
 
     private View mView;
 
+    private NewPaymentCallback newPaymentCallback;
+
     @BindView(R.id.card_input_widget) CardInputWidget card_input_widget;
     @BindView(R.id.btn_save_payment) Button btn_save_payment;
+    @BindView(R.id.txt_view_cancel_add_payment) TextView txt_view_cancel_add_payment;
 
     private Card card;
     private Stripe stripe;
     private Customer customer;
 
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof NewPaymentFragment.NewPaymentCallback) {
+            newPaymentCallback = (NewPaymentFragment.NewPaymentCallback) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement NewPaymentFragmentInteractionListener");
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,9 +60,11 @@ public class PaymentFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.activity_payment, container, false);
+
         ButterKnife.bind(this, mView);
 
         btn_save_payment.setOnClickListener(this);
+        txt_view_cancel_add_payment.setOnClickListener(this);
 
         return mView;
     }
@@ -59,7 +72,7 @@ public class PaymentFragment extends Fragment
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
 
-        getActivity().setTitle(getResources().getString(R.string.payments_nav_title));
+        getActivity().setTitle(getResources().getString(R.string.payments_add_new_title));
     }
 
     @Override
@@ -68,7 +81,6 @@ public class PaymentFragment extends Fragment
             case R.id.btn_save_payment:
 
                 //TODO: Save customer ID for future charges
-
                 card = card_input_widget.getCard();
                 if (card == null) {
                     return;
@@ -90,6 +102,15 @@ public class PaymentFragment extends Fragment
                                 }
                             });
                 }
+                break;
+
+            case R.id.txt_view_cancel_add_payment:
+                newPaymentCallback.navigateToPaymentListings();
+                break;
         }
+    }
+
+    public interface NewPaymentCallback {
+        void navigateToPaymentListings();
     }
 }
