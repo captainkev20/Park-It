e1610326a6a5971494572b355a8ded0794997f38
Kevin Walker <kevin.walker@droid.rocks>
2019-10-15 04:57:48 -0400

    Add ParkItActivity and ParkItPresenter base files

---
+++ app/src/main/java/com/rocks/kevinwalker/parkit/ParkItActivity.java
@@ -0,0 +1,7 @@
+package com.rocks.kevinwalker.parkit;
+
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class ParkItActivity<T extends ParkItPresenter> extends AppCompatActivity {
+
+}
